1fa5acabac63c0b47ef86fa367cc28bf3fa46ca4
James Golick <james@giraffesoft.ca>
2009-09-28 11:36:14 -0700

    ugh... api knowledge fail

--- cookbooks/deployment/recipes/default.rb
+++ cookbooks/deployment/recipes/default.rb
@@ -2,13 +2,13 @@ $LOAD_PATH << File.expand_path("../../../../vendor/chef-deploy/lib", __FILE__)
 require 'chef-deploy'
 
 deploy "/var/www/#{@node[:apps].first}" do
-  repo            @node[:app_clone_url]
-  user            @node[:user]
-  role            @node[:role]
-  environment     @node[:rails_env]
-  migrate         true if @node[:master]
-  migrate_command "rake db:migrate"
-  restart_command "sudo /opt/ruby-enterprise/bin/god restart mongrels" if @node[:role] == "app" && @node[:configured]
-  action          :deploy
+  repo              @node[:app_clone_url]
+  user              @node[:user]
+  role              @node[:role]
+  environment       @node[:rails_env]
+  migrate           true if @node[:master]
+  migration_command "rake db:migrate"
+  restart_command   "sudo /opt/ruby-enterprise/bin/god restart mongrels" if @node[:role] == "app" && @node[:configured]
+  action            :deploy
 end
 
