1c16a1e8109ad8715a9c85d12372ce6e85d196f4
James Golick <james@giraffesoft.ca>
2009-09-28 12:07:29 -0700

    shift order

--- roles/app.rb
+++ roles/app.rb
@@ -1,5 +1,5 @@
 name "app"
 recipes "users", "god", "haproxy", "logrotate", "mysql::client", 
-        "gems", "database.yml", "nginx", "memcached", "applications",
+        "gems", "nginx", "applications", "memcached", "database.yml",
         "deployment" # rails-deployment really needs to happen last
 
