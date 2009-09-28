ff82077bf740da65bd1d02a5a7c6f2aa40d83d05
James Golick <james@giraffesoft.ca>
2009-09-28 11:30:37 -0700

    add mgiration command

--- cookbooks/deployment/recipes/default.rb
+++ cookbooks/deployment/recipes/default.rb
@@ -7,6 +7,7 @@ deploy "/var/www/#{@node[:apps].first}" do
   role            @node[:role]
   environment     @node[:rails_env]
   migrate         true if @node[:master]
+  migrate_command "rake db:migrate"
   restart_command "sudo /opt/ruby-enterprise/bin/god restart mongrels" if @node[:role] == "app" && @node[:configured]
   action          :deploy
 end
