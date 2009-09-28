0473b4e02d7b580f3b78ecf607ecab3c3ba8858c
James Golick <james@giraffesoft.ca>
2009-09-28 10:21:25 -0700

    reorganize recipe order

--- roles/app.rb
+++ roles/app.rb
@@ -1,5 +1,5 @@
 name "app"
-recipes "users", "god", "haproxy", "logrotate", "applications",
-        "mysql::client", "gems", "database.yml", "nginx", 
-        "memcached", "deployment" # rails-deployment really needs to happen last
+recipes "users", "god", "haproxy", "logrotate", "mysql::client", 
+        "gems", "database.yml", "nginx", "memcached", "applications",
+        "deployment" # rails-deployment really needs to happen last
 
