52f9824eab8374b1e72e1c541148755d2490a0fd
James Golick <james@giraffesoft.ca>
2009-09-28 09:40:29 -0700

    add users cookbook

---
+++ cookbooks/users/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: users
+# Recipe:: default
+#
+# Copyright 2009, Protose, Inc.
+#
+# Distributable under the terms of the MIT License.
+#
+
+@node[:apps].each do |app|
+  user app do
+    gid   app
+    home  "/home/#{app}"
+    shell "/usr/bin/bash"
+  end
+end
+
