df1695f02b076f666c66ca9b66f777099441ddc0
James Golick <james@giraffesoft.ca>
2009-09-28 09:01:01 -0700

    import attributes.rb

---
+++ attributes.rb
@@ -0,0 +1,13 @@
+innodb_buffer_pool_size "1G"
+sort_buffer_size "256K"
+db_username "dbuser"
+db_password "134asdfjagj23nzvz9gq43ZXFASDGf"
+mysql_ec2_path "/mnt/mysql"
+gems [{:name => "rdiscount"},
+      {:name => "mysql", :version => "2.7"},
+      {:name => "xml-simple", :version => "1.0.12"},
+      {:name => "right_aws"},
+      {:name => "rails", :version => "2.3.3"},
+      {:name => "mime-types"},
+      {:name => "sinatra"}]
+
