18838bff88d3be0e9336a2b31e2b5e85d1a6e2e1
James Golick <james@giraffesoft.ca>
2009-09-28 09:41:03 -0700

    strip down mysql master role

--- roles/mysql_master.rb
+++ roles/mysql_master.rb
@@ -1,4 +1,4 @@
 name "mysql_master"
 description "Master mysql servers"
-recipes "vim", "dotfiles", "logrotate", "mysql::server",
-        "ncurses-term", "ebs_snapshots", "nagios::nrpe-server"
+recipes "logrotate", "mysql::server"
+        
